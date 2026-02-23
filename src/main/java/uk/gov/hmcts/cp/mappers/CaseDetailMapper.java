eed11d1294d1e4cfaa81a70a40095bca9499bd4b
vineetkapoor-hmcts <vineet.kapoor@hmcts.net>
2026-02-23 09:30:06 +0000

    Clean up the repo

---
+++ src/main/java/uk/gov/hmcts/cp/mappers/CaseDetailMapper.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.cp.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CaseDetailMapper {
+
+}
--- src/test/java/uk/gov/hmcts/cp/filters/tracing/TracingFilterTest.java
+++ src/test/java/uk/gov/hmcts/cp/filters/tracing/TracingFilterTest.java
@@ -35,7 +35,7 @@ class TracingFilterTest {
         when(request.getHeader(TRACE_ID)).thenReturn("incoming-traceId");
         when(request.getHeader(SPAN_ID)).thenReturn("incoming-spanId");
 
-        tracingFilter.populateMDC(request, response, filterChain);
+        tracingFilter.doFilterInternal(request, response, filterChain);
 
         assertThat(MDC.get(APPLICATION_NAME)).isEqualTo("myAppName");
         verify(response).setHeader(TRACE_ID, "incoming-traceId");
